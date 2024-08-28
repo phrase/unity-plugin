7e641c719c22e0e06a9395017948ee8f3098d397
Mladen Jablanovic <mladen.jablanovic@phrase.com>
2024-08-28 09:44:50 +0200

    Clean up the client a bit

--- Editor/PhraseClient.cs
+++ Editor/PhraseClient.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -154,7 +153,7 @@ namespace Phrase
             return JsonConvert.DeserializeObject<List<Project>>(jsonResponse);
         }
 
-        public void UploadFile(string translations, string projectID, string localeID, bool autoTranslate)
+        public async void UploadFile(string path, string projectID, string localeID, bool autoTranslate)
         {
             string url = string.Format("{0}/projects/{1}/uploads", ApiUrl, projectID);
             NameValueCollection nvc = new NameValueCollection();
@@ -163,91 +162,37 @@ namespace Phrase
             nvc.Add("autotranslate", autoTranslate.ToString());
             nvc.Add("format_options[key_name_attribute]", "resname");
 
-            byte[] content = Encoding.ASCII.GetBytes(translations);
-
-            HttpUploadFile(url, "translations.xlf", "file", "text/plain", nvc, content);
+            await HttpUploadFile(url, path, "file", "text/plain", nvc);
         }
 
-        public Screenshot UploadScreenshot(byte[] image, string projectID, string localeID)
+        // public async Task<Screenshot> UploadScreenshot(byte[] image, string projectID, string localeID)
+        // {
+        //     string url = string.Format("{0}/projects/{1}/screenshots", ApiUrl, projectID);
+        //     NameValueCollection nvc = new NameValueCollection();
+        //     var response = await HttpUploadFile(url, "unity.jpg", "filename", "image/jpeg", nvc, image);
+        //     var screenshot = JsonUtility.FromJson<Screenshot>(response);
+        //     return screenshot;
+        // }
+
+        private async Task<string> HttpUploadFile(string url, string path, string paramName, string contentType, NameValueCollection nvc)
         {
-            string url = string.Format("{0}/projects/{1}/screenshots", ApiUrl, projectID);
-            NameValueCollection nvc = new NameValueCollection();
-            var response = HttpUploadFile(url, "unity.jpg", "filename", "image/jpeg", nvc, image);
-            var screenshot = JsonUtility.FromJson<Screenshot>(response);
-            return screenshot;
-        }
+            MultipartFormDataContent form = new MultipartFormDataContent();
+            var fileStream = new FileStream(path, FileMode.Open);
+            var streamContent = new StreamContent(fileStream);
+            streamContent.Headers.Add("Content-Type", contentType);
+            var fileName = Path.GetFileName(path);
+            form.Add(streamContent, paramName, fileName);
 
-        string HttpUploadFile(string url, string file, string paramName, string contentType, NameValueCollection nvc, byte[] content)
-        {
-            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
-            byte[] boundarybytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
-
-            HttpWebRequest wr = CreateRequest(url);
-            wr.ContentType = "multipart/form-data; boundary=" + boundary;
-            wr.Method = "POST";
-            wr.KeepAlive = true;
-
-            Stream rs = wr.GetRequestStream();
-
-            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
             foreach (string key in nvc.Keys)
             {
-                rs.Write(boundarybytes, 0, boundarybytes.Length);
-                string formitem = string.Format(formdataTemplate, key.Replace(@"\", @"\\").Replace(@"""", @"\"""), nvc[key]);
-                byte[] formitembytes = Encoding.UTF8.GetBytes(formitem);
-                rs.Write(formitembytes, 0, formitembytes.Length);
-            }
-            rs.Write(boundarybytes, 0, boundarybytes.Length);
-
-            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
-            string header = string.Format(headerTemplate, paramName, file, contentType);
-            byte[] headerbytes = Encoding.UTF8.GetBytes(header);
-            rs.Write(headerbytes, 0, headerbytes.Length);
-            rs.Write(content, 0, content.Length);
-
-            byte[] trailer = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
-            rs.Write(trailer, 0, trailer.Length);
-            rs.Close();
-
-            WebResponse wresp = null;
-            try
-            {
-                wresp = wr.GetResponse();
-                Stream stream2 = wresp.GetResponseStream();
-                StreamReader reader2 = new StreamReader(stream2);
-                var response = reader2.ReadToEnd();
-                Provider.Log(string.Format("File uploaded, server response is: {0}", response));
-                return response;
-            }
-            catch (WebException ex)
-            {
-                using (var stream = ex?.Response?.GetResponseStream())
-                    if (stream != null)
-                        using (var reader = new StreamReader(stream))
-                        {
-                            Provider.Log(reader.ReadToEnd());
-                        }
-
-                Provider.LogError("Error uploading file" + ex.ToString());
-                if (wresp != null)
-                {
-                    Provider.Log("not null");
-                    using (var reader = new StreamReader(wresp.GetResponseStream()))
-                    {
-                        string result = reader.ReadToEnd(); // do something fun...
-                        Provider.Log("test");
-                        Provider.Log(result);
-                    }
-                    wresp.Close();
-                    wresp = null;
-                }
-            }
-            finally
-            {
-                wr = null;
+                form.Add(new StringContent(nvc[key]), key);
             }
 
-            return null;
+            var response = await Client.PostAsync(url, form);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Provider.Log("Response: " + responseContent);
+            response.EnsureSuccessStatusCode();
+            return Task.FromResult(responseContent).Result;
         }
     }
 }
